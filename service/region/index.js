bc60564d3e9e29b099ecccc425f4018cd7259860
SnakeRuse <snakeice2000@mail.ru>
2023-06-12 11:57:41 +0300

    create&get region service

---
+++ service/region/index.js
@@ -0,0 +1,28 @@
+const ApiError = require("../../exception");
+const { db } = require("../../orm");
+
+async function get({ name }) {
+  const region = await db.Region.findOne({
+    where: { name }
+  });
+  if (!region) {
+    throw ApiError.NotFound();
+  };
+  return region;
+}
+
+async function create(stateName, name) {
+  const candidate = await db.Region.findOne({
+    where: { name }
+  });
+  if (candidate) {
+    throw ApiError.AlreadyExist();
+  };
+  const region = await db.Region.create({ name, stateName })
+  return region;
+}
+
+module.exports = {
+  get, 
+  create,
+}
